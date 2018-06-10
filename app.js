ccf58686f5e4168dcec411e385c632ebfb41cb95
Carolinadelb <carolinadelgadob@gmail.com>
2018-06-10 17:24:24 -0400

    login con nueva función

---
+++ app.js
@@ -0,0 +1,20 @@
+/*function go(){
+    if (document.form.password.value ==='123' && document.form.login.value ==='Valentina'){ 
+            document.form.submit(); 
+        } 
+        else{ 
+             alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+        } 
+    } 
+*/
+
+    let go = () => {
+        if (document.form.password.value ==='123' && document.form.login.value ==='Valentina'){ 
+                document.form.submit(); 
+            } 
+            else{ 
+                 alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+            } 
+        } 
+
+        
